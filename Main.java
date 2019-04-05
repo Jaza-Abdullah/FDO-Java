53a30d619234a5e01a26be6c55722cc66cac458b
Jaza-Abdullah <49320156+Jaza-Abdullah@users.noreply.github.com>
2019-04-05 21:17:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  * @author Jaza Mahmood Abdullah
  * Apache License Version 2.0, January 2004
  * http://www.apache.org/licenses/
+
+ Main Paper 
+ J. M. Abdullah and T. A. Rashid, "Fitness Dependent Optimizer: Inspired by the Bee Swarming Reproductive Process," in IEEE Access.
+doi: 10.1109/ACCESS.2019.2907012
+keywords: {Optimization;Heuristic algorithms;Classification algorithms;Genetic algorithms;Artificial bee colony algorithm;Particle swarm optimization;Optimization;Swarm Intelligence;Evolutionary Computation;Metaheuristic Algorithms;Fitness Dependent Optimizer;FDO},
+URL: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8672851&isnumber=6514899
  */
+
+
+
 
 //you can use FX1, FX2 ... FX19,FXantena and function_FM for functionName
 public class Main {
